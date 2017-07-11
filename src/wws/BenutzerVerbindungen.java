257685c9e4dd0404bcdb1644f77ac6748feb4e1e
Mat <Mat@192.168.151.136>
2017-07-11 09:38:39 +0200

    Hallo eingefügt

--- src/wws/BenutzerVerbindungen.java
+++ src/wws/BenutzerVerbindungen.java
@@ -117,7 +117,7 @@ public class BenutzerVerbindungen extends javax.swing.JFrame {
         ));
         jScrollPane2.setViewportView(jTable1);
 
-        jLabel1.setText("DB Verbindungen");
+        jLabel1.setText("DB Verbindungen Hallo");
 
         jButtonDBLoeschen.setText("Löschen");
         jButtonDBLoeschen.addActionListener(new java.awt.event.ActionListener() {
